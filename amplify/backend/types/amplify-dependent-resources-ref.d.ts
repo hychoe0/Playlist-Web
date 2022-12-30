d547d92656a05b0dfe2a14ddb03cbe83353c80c9
hychoe0 <hychoe@umich.edu>
2022-12-29 23:29:29 -0500

    Added aws-exports.js and amplify-cli

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "playlistweb079ad2a0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
