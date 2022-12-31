f87bd5de5e8c76f69ced6710d9d60f358523fb8d
hychoe0 <hychoe@umich.edu>
2022-12-31 18:07:57 -0500

    Deleted credentials

--- src/Credential.js
+++
@@ -1,8 +0,0 @@
-const Credentials = () => {
-  return {
-    ClientId: "49c7ad3f813c40b6a8fb8bf68c99839a",
-    ClientSecret: "b5411f07013349b9b97e633859093366",
-  };
-};
-
-export { Credentials };
